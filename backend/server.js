768b73ae4a63589abf5cb0dfbadc26bfe114f2c9
Rahul <rahulna1996@gmail.com>
2024-04-14 10:34:49 +0530

    connected DB and created user Schema

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const PORT = 6969;
 const app = express();
+app.use(express.json());
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/user-Auth")
+  .then(() => console.log("DB Connected!"));
+
+const userSchema = new mongoose.Schema({
+  userName: String,
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, resp) => {
   console.log("GET API called!");
